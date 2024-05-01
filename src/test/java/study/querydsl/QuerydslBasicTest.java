a98405604a8b0fded7ee88e57b8e27de64d3c928
chanhl22 <lch9502@naver.com>
2024-05-01 22:35:58 +0900

    test: 상수, concat

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -4,6 +4,7 @@ import com.querydsl.core.NonUniqueResultException;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -459,6 +460,31 @@ public class QuerydslBasicTest {
             Integer rank = tuple.get(rankPath);
             System.out.println("username = " + username + " age = " + age + " rank = " + rank);
         }
+    }
+
+    @DisplayName("상수 사용하기")
+    @Test
+    void constant() {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @DisplayName("문자 더하기")
+    @Test
+    void concat() {
+        String result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        System.out.println("result = " + result);
     }
 
 }
